455a3f4fec8bd5eb8e1caae3a07ea7f325aae616
lalilaloe <jonathan@superbuddy.nl>
2018-12-19 15:28:43 +0100

    Default crypto module test

--- src/HTTP/EncryptRequestHandler.ts
+++ src/HTTP/EncryptRequestHandler.ts
@@ -1,15 +1,19 @@
 import * as forge from "node-forge";
-import { encryptString } from "../Crypto/Sha256";
 import BunqJSClient from "../BunqJSClient";
 import Session from "../Session";
 import LoggerInterface from "../Interfaces/LoggerInterface";
 import Request from "./Request";
+var crypto = require('crypto');
 
 import ApiAdapterOptions from "../Types/ApiAdapterOptions";
 
 const HEADER_CLIENT_ENCRYPTION_HMAC = "X-Bunq-Client-Encryption-Hmac";
 const HEADER_CLIENT_ENCRYPTION_IV = "X-Bunq-Client-Encryption-Iv";
 const HEADER_CLIENT_ENCRYPTION_KEY = "X-Bunq-Client-Encryption-Key";
+const AES_ENCRYPTION_METHOD = 'aes-256-cbc';
+const AES_KEY_LENGTH = 32;
+const HMAC_ALGORITHM = 'sha1';
+const INITIATION_VECTOR_LENGTH = 16;
 
 export default class EncryptRequestHandler {
     public Session: Session;
@@ -31,55 +35,52 @@ export default class EncryptRequestHandler {
     public async encryptRequest(request: Request, options: ApiAdapterOptions): Promise<void> {
         const body = JSON.stringify(request.requestConfig.data);
 
-        const iv = forge.random.getBytesSync(16);
-        const key = forge.random.getBytesSync(16);
+        const key = crypto.randomBytes(AES_KEY_LENGTH);
+        const iv = crypto.randomBytes(INITIATION_VECTOR_LENGTH);
 
-        const encryptedAesKey = await encryptString(key, this.Session.serverPublicKey, true);
-        const encryptedBody: string = this.getEncryptedBody(key, iv, body);
-        const hmacBytes: string = this.getBodyHmac(key, iv, encryptedBody);
+        const encryptedAesKey = this.encryptPublic(key, this.Session.serverPublicKey);
+        const encryptedBody = this.encrypt(body, key, iv);
+        const hmacBuffer = this.hmac(key, iv + encryptedBody);
 
         // set new body
-        request.setData(encryptedBody);
+        request.setData(encryptedBody.toString('base64'));
 
         // disable request transform
-        request.setOptions("transformRequest", (data: any, headers: any) => {
+        request.setOptions("transformRequest", data => {
+            // don't transform the data, return it directly
             return data;
         });
 
+        console.log("Hmac\n")
+        console.log(hmacBuffer)
+        console.log("IV\n")
+        console.log(iv)
+        console.log("Key\n")
+        console.log(encryptedAesKey)
+
         // set headers
-        request.setHeader(HEADER_CLIENT_ENCRYPTION_HMAC, forge.util.encode64(hmacBytes));
-        request.setHeader(HEADER_CLIENT_ENCRYPTION_IV, forge.util.encode64(iv));
-        request.setHeader(HEADER_CLIENT_ENCRYPTION_KEY, forge.util.encode64(encryptedAesKey));
+        request.setHeader("Content-Type", "multipart/form-data");
+        request.setHeader(HEADER_CLIENT_ENCRYPTION_HMAC, hmacBuffer);
+        request.setHeader(HEADER_CLIENT_ENCRYPTION_IV, iv);
+        request.setHeader(HEADER_CLIENT_ENCRYPTION_KEY, encryptedAesKey);
     }
 
-    /**
-     * @param {string} key
-     * @param {string} iv
-     * @param {string} body
-     */
-    private getEncryptedBody(key: string, iv: string, body: string): string {
-        const cipher = forge.cipher.createCipher("AES-CBC", key);
-
-        const buffer = forge.util.createBuffer(body, "raw");
-        cipher.start({ iv: iv });
-        cipher.update(buffer);
-        cipher.finish();
-
-        return cipher.output.getBytes();
-    }
-
-    /**
-     * @param {string} key
-     * @param {string} iv
-     * @param {string} encryptedBody
-     */
-    private getBodyHmac(key: string, iv: string, encryptedBody: string): string {
-        const mergedContent = iv + encryptedBody;
-
-        const hmac = forge.hmac.create();
-        hmac.start("sha1", key);
-        hmac.update(mergedContent);
-
+    private hmac(key, content) {
+        var hmac = crypto.createHmac(HMAC_ALGORITHM, key);
+        hmac.update(content);
         return hmac.digest().getBytes();
+    }
+
+    private encrypt(text, key, iv) {
+        var cipher = crypto.createCipheriv(AES_ENCRYPTION_METHOD, key, iv)
+        var crypted = cipher.update(text)
+        crypted += cipher.final('raw');
+        return crypted.getBytes();
+    }
+
+    private encryptPublic(key, publicKey) {
+        const messageDigest = forge.md.sha256.create();
+        messageDigest.update(key, 'raw')
+        return publicKey.encrypt(messageDigest.digest().getBytes());
     }
 }
