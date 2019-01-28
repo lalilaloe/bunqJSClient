0505ccfedcf8c6e060c3dd7b4a8db648db2b096a
lalilaloe <jonathan@superbuddy.nl>
2019-01-28 10:16:01 +0100

    Reformat encryption test

--- tests/src/HTTP/EncryptRequestHandler.test.ts
+++ tests/src/HTTP/EncryptRequestHandler.test.ts
@@ -20,72 +20,64 @@ const request = new Request(
 );
 
 describe("EncryptRequest", () => {
-	beforeEach(function () {
+	let bunqApp: BunqJSClient;
+	let handler: EncryptRequestHandler;
+	let server_private_key: any;
+	let encryption_data: Request;
+	let encrypted_body: string;
+	let headers: any;
+	let iv: string;
+	let encrypted_key: string;
+
+	beforeEach(async function () {
 		moxios.install();
+		bunqApp = await SetupApp();
+
+		server_private_key = await privateKeyFromPem(process.env.CI_SERVER_PRIVATE_KEY_PEM);
+		handler = bunqApp.ApiAdapter.EncryptRequestHandler;
+		encryption_data = await handler.encryptRequest(request, {});
+		encrypted_body = encryption_data.requestConfig.data;
+		headers = encryption_data.requestConfig.headers;
+		iv = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_IV]);
+		encrypted_key = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_KEY]);
 	});
 
 	afterEach(function () {
 		moxios.uninstall();
 	});
+	it("allows decryption using server private key", async () => {
+		expect(server_private_key).toBeDefined();
+		expect(encryption_data).toBeDefined();
+		expect(encrypted_body).toBeDefined();
+		expect(iv).toBeDefined();
+		expect(encrypted_key).toBeDefined();
 
-	describe("#encrypt", () => {
-		it("allows decryption using server private key", async () => {
-			const bunqApp: BunqJSClient = await SetupApp();
-			//expect.assertions(1);
+		const key = server_private_key.decrypt(encrypted_key);
+		expect(key).toBeDefined();
 
-			let server_private_key = await privateKeyFromPem(process.env.CI_SERVER_PRIVATE_KEY_PEM);
-			let handler = bunqApp.ApiAdapter.EncryptRequestHandler;
-			let encryption_data = await handler.encryptRequest(request, {});
-			let encrypted_body = encryption_data.requestConfig.data;
-			let headers = encryption_data.requestConfig.headers;
-			let iv = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_IV]);
-			let encrypted_key = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_KEY]);
+		var data = forge.util.createBuffer(encrypted_body);
+		var decipher = forge.cipher.createDecipher('AES-CBC', key);
+		decipher.start({ iv: iv });
+		decipher.update(data);
+		decipher.finish();
+		var decrypted = JSON.parse(decipher.output.data);
+		expect(decrypted).toEqual(body);
+	});
 
+	it("returns a valid HMAC", async () => {
+		let hmac = forge.util.decode64(encryption_data.requestConfig.headers[handler.HEADER_CLIENT_ENCRYPTION_HMAC]);
 
-			expect(server_private_key).toBeDefined();
-			expect(encryption_data).toBeDefined();
-			expect(encrypted_body).toBeDefined();
-			expect(iv).toBeDefined();
-			expect(encrypted_key).toBeDefined();
+		expect(server_private_key).toBeDefined();
+		expect(encryption_data).toBeDefined();
+		expect(encrypted_body).toBeDefined();
+		expect(iv).toBeDefined();
+		expect(encrypted_key).toBeDefined();
 
-			const key = server_private_key.decrypt(encrypted_key);
-			expect(key).toBeDefined();
+		const key = server_private_key.decrypt(encrypted_key);
+		expect(key).toBeDefined();
 
-			var data = forge.util.createBuffer(encrypted_body);
-			var decipher = forge.cipher.createDecipher('AES-CBC', key);
-			decipher.start({ iv: iv });
-			decipher.update(data);
-			decipher.finish();
-			var decrypted = JSON.parse(decipher.output.data);
-			expect(decrypted).toEqual(body);
-		});
+		let test_hmac = handler.hmac(key, iv + encrypted_body)
 
-		it("returns a valid HMAC", async () => {
-			const bunqApp: BunqJSClient = await SetupApp();
-			//expect.assertions(1);
-
-			let server_private_key = await privateKeyFromPem(process.env.CI_SERVER_PRIVATE_KEY_PEM);
-			let handler = bunqApp.ApiAdapter.EncryptRequestHandler;
-			let encryption_data = await handler.encryptRequest(request, {});
-			let encrypted_body = encryption_data.requestConfig.data;
-			let headers = encryption_data.requestConfig.headers;
-			let iv = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_IV]);
-			let encrypted_key = forge.util.decode64(headers[handler.HEADER_CLIENT_ENCRYPTION_KEY]);
-
-			let hmac = forge.util.decode64(encryption_data.requestConfig.headers[handler.HEADER_CLIENT_ENCRYPTION_HMAC]);
-
-			expect(server_private_key).toBeDefined();
-			expect(encryption_data).toBeDefined();
-			expect(encrypted_body).toBeDefined();
-			expect(iv).toBeDefined();
-			expect(encrypted_key).toBeDefined();
-
-			const key = server_private_key.decrypt(encrypted_key);
-			expect(key).toBeDefined();
-
-			let test_hmac = handler.hmac(key, iv + encrypted_body)
-
-			expect(hmac).toEqual(test_hmac);
-		});
+		expect(hmac).toEqual(test_hmac);
 	});
 });
